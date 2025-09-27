177d8427138904bee35a90b36bfbebd6d5d52f65
rezgui99 <mohammedaziz.rezgui@esprit.tn>
2025-09-27 06:09:00 +0100

    Premier commit11

--- zz/fast_api/app/models/single_calculation_request.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-from app.models.fiche_poste import JobDescription
-from app.models.fiche_employe import Employee
-
-class SingleCalculationRequest(BaseModel):
-    job_description: JobDescription
-    employee: Employee
